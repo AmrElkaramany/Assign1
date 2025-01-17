22ddc1b300cb4f2f2f5d411f87856ef88fe2c85f
Amr Taha <amr.route@gmail.com>
2025-01-17 16:34:59 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -11,7 +11,7 @@ import Contact from "./Components/Contact/Contact";
 import Notfound from "./Components/NotFound/Notfound";
 import Portfolio from "./Components/Portfolio/Portfolio";
 
-let router = createBrowserRouter([
+let router = createHashRouter([
   {
     path: "",
     element: <Layout />,
